e833e6b381460480bdd09dd08792196f607edf4d
jansdhillon <77344313+jansdhillon@users.noreply.github.com>
2023-04-11 20:51:29 -0700

    Initial Commit

---
+++ AlMacA/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AlMacA
+//
+//  Created by Jan Dhillon on 2023-04-11.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
